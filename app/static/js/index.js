517b2bb3c7411932b09846edf4db8cd72116646b
Mathieu <math.baffoigne@gmail.com>
2024-02-06 09:15:16 +0100

    quick fix

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -87,7 +87,7 @@ const sendKeypointsToBackend = async (keypoints, dataUrl) => {
   formData.append('image', blob, 'image.jpg');
 
   try {
-    const response = await fetch('http://127.0.0.1:8000/predict', {
+    const response = await fetch('https://asl-web-app.onrender.com/predict', {
       method: 'POST',
       body: formData
     });
