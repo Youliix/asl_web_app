dbb4729d960cbeeac0290184af83cdc1481d7080
Mathieu <math.baffoigne@gmail.com>
2024-02-12 23:25:17 +0100

    update to finish

--- app/endpoint.py
+++ app/endpoint.py
@@ -9,13 +9,16 @@ from .db import save_image_content
 
 endpoint = Blueprint('endpoint', __name__)
 
-model = joblib.load('app/static/src/model/model_xgb_xy_angles_only_v2.pkl')
+model = joblib.load('app/static/src/model/work/model_xgb_xyz_angles_only_v1.pkl')
 
-class_names = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-joint_list = [[4,3,2], [8,7,6], [12,11,10], [16,15,14], [20,19,18]]
-joint_list_ext = [[4,1], [8,5], [12,9], [16,13], [20,17]]
-column_names = [f"{dim}{i}" for i in range(1, 22) for dim in ['x', 'y', 'z']] + [f"angle{j}" for j in range(1, 6)]
-column_angles = [f"Angle_{j}" for j in range(1, 6)]
+class_names = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y']
+
+# joint_list = [[4,3,2], [8,7,6], [12,11,10], [16,15,14], [20,19,18]]
+joint_list = [[4,1], [8,5], [12,9], [16,13], [20,17]]
+
+# column_names = [f"{dim}{i}" for i in range(1, 22) for dim in ['x', 'y', 'z']] + [f"angle{j}" for j in range(1, 6)]
+column_angles = [f"angle_{j}" for j in range(0, 5)]
+
 
 @endpoint.route('/')
 def home():
@@ -26,11 +29,9 @@ def home():
 def predict():
     try:
         keypoints = request.form['keypoints']   
+        keypoints = eval(keypoints)
         keypoints_angles = draw_finger_angles_by_base_of_wrist(keypoints)
-        # keypoints_angles = draw_finger_angles(keypoints, joint_list)
-        # logging.warning(keypoints_angles)
-        # prediction = predict_class(keypoints, keypoints_angles)
-        prediction = predict_class(keypoints_angles)
+        prediction = predict_class(keypoints, keypoints_angles)
         # img = request.files['image']
         # save_content(img, keypoints, prediction)
     except Exception as e:
@@ -39,14 +40,29 @@ def predict():
         return jsonify({'letter': prediction})
 
 
-# def predict_class(keypoints, keypoints_angles):
-def predict_class(keypoints_angles):
-    # keypoints = eval(keypoints)
-    # coordinates_flat_list = [value for point in keypoints for value in point.values()]
-    # final_list = coordinates_flat_list + keypoints_angles
-    df = pd.DataFrame([keypoints_angles])
-    # logging.warning(df)
-    prediction = model.predict(df)
+def predict_class(keypoints, keypoints_angles):
+    for keypoint in keypoints:
+        del keypoint['z']
+    keypoints_indices = [0, 4, 8, 12, 16, 20]
+    new_keypoints = [keypoints[i] for i in keypoints_indices]
+    keypoints = [item for sublist in keypoints for item in sublist.values()]
+    
+    s = pd.Series(keypoints_angles , keypoints)
+    # df = pd.DataFrame([keypoints])
+    # new_column_names = {}
+    # for i, index in enumerate(keypoints_indices):
+    #     new_column_names[str(i)] = f'x_{index}'
+    #     new_column_names[str(i+1)] = f'y_{index}'
+
+    # df.rename(columns=new_column_names, inplace=True)
+    # df = df.drop(columns=keypoints_indices)
+    # for i, index in enumerate(keypoints_indices):
+    #     df[f'x_{index}'] = df['keypoints'].apply(lambda x: x[i][0] if isinstance(x, list) and len(x) > i and isinstance(x[i], tuple) else None)
+    #     df[f'y_{index}'] = df['keypoints'].apply(lambda x: x[i][1] if isinstance(x, list) and len(x) > i and isinstance(x[i], tuple) else None)
+    # df = df.drop(columns=['keypoints'])    
+    # df = pd.DataFrame([keypoints_angles], columns=column_angles)
+    # df = pd.concat([pd.DataFrame([keypoints_angles], columns=column_angles), df], axis=1)
+    prediction = model.predict(s)
     result = class_names[prediction[0]]
     return result
 
@@ -55,27 +71,27 @@ def save_content(img, keypoints, prediction):
     save_image_content(img, keypoints, prediction)
 
 
-def draw_finger_angles(keypoints):
-    keypoints = eval(keypoints)
-    list_angles = []
-    for joint in joint_list:
-        a = keypoints[joint[0]]
-        b = keypoints[joint[1]]
-        c = keypoints[joint[2]]
-        radians = np.arctan2(c['y'] - b['y'], c['x'] - b['x']) - np.arctan2(a['y'] - b['y'], a['x'] - b['x'])
-        angle = np.abs(radians * 180.0 / np.pi)
-        if angle > 180.0:
-            angle = 360 - angle
-        list_angles.append(angle)
-    return list_angles
+# def draw_finger_angles(keypoints):
+#     keypoints = eval(keypoints)
+#     list_angles = []
+#     for joint in joint_list:
+#         a = keypoints[joint[0]]
+#         b = keypoints[joint[1]]
+#         c = keypoints[joint[2]]
+#         radians = np.arctan2(c['y'] - b['y'], c['x'] - b['x']) - np.arctan2(a['y'] - b['y'], a['x'] - b['x'])
+#         angle = np.abs(radians * 180.0 / np.pi)
+#         if angle > 180.0:
+#             angle = 360 - angle
+#         list_angles.append(angle)
+#     return list_angles
 
 
 def draw_finger_angles_by_base_of_wrist(keypoints):
     angles = []
-    for joint in joint_list_ext:
-        a = (keypoints[0])
-        b = (keypoints[joint[1]])
-        c = (keypoints[joint[0]])
+    for joint in joint_list:
+        a = (keypoints[0]['x'], keypoints[0]['y'])
+        b = (keypoints[joint[1]]['x'], keypoints[joint[1]]['y'])
+        c = (keypoints[joint[0]]['x'], keypoints[joint[0]]['y'])
         angle = calculate_angle(a, b, c)
         angles.append(angle)
     return angles
