b870bc075ef04a2cb00e9742ce1054684c9fda8d
Mathieu <math.baffoigne@gmail.com>
2024-02-16 11:29:53 +0100

    Web app fixed

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,34 +1,23 @@
-import { FilesetResolver, HandLandmarker } from 'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.9/+esm';
+import {
+  FilesetResolver,
+  HandLandmarker,
+} from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.9/+esm";
 
-let BASE_URL = 'https://asl-web-app.onrender.com';
+let BASE_URL = "https://asl-web-app.onrender.com";
 let handLandmarker;
-let runningMode = 'VIDEO';
-let predictedLetter = '';
-const spinner = document.querySelector('.fa-spinner');
-
-const createHandLandmarker = async () => {
-  const vision = await FilesetResolver.forVisionTasks('https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm');
-  handLandmarker = await HandLandmarker.createFromOptions(vision, {
-    baseOptions: {
-      modelAssetPath: 'https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task',
-      delegate: "GPU",
-    },
-    runningMode: runningMode,
-    numHands: 1,
-    minHandDetectionConfidence: 0.8,
-    minTrackingConfidence: 0.8,
-    minHandPresenceConfidence: 0.8
-  });
-  enableWebcam();
-};
-createHandLandmarker();
+let runningMode = "VIDEO";
+let predictedLetter = "";
 
 let webcamRunning = false;
-const video = document.getElementById('video');
-const canvas = document.getElementById('canvas');
-const canvasCtx = canvas.getContext('2d');
+let lastVideoTime = -1;
+let results = undefined;
 
-const adjustCanvasSize = () => {  
+const video = document.getElementById("video");
+const canvas = document.getElementById("canvas");
+const canvasCtx = canvas.getContext("2d");
+const spinner = document.querySelector(".fa-spinner");
+
+const adjustCanvasSize = () => {
   if (video.videoWidth > 0 && video.videoHeight > 0) {
     const aspectRatio = video.videoWidth / video.videoHeight;
     const displayWidth = video.offsetWidth;
@@ -36,23 +25,22 @@ const adjustCanvasSize = () => {
     canvas.width = displayWidth;
     canvas.height = displayHeight;
   }
-  console.log('Canvas size:', canvas.width, canvas.height);
 };
 
-window.addEventListener('resize', adjustCanvasSize);
+window.addEventListener("resize", adjustCanvasSize);
 
 const enableWebcam = async () => {
   webcamRunning = true;
 
   const constraints = {
-    video: true
+    video: true,
   };
 
   await navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
     video.srcObject = stream;
     video.addEventListener("loadeddata", () => {
       if (video.readyState >= 3) {
-        spinner.style.display = 'none';
+        spinner.style.display = "none";
       }
       adjustCanvasSize();
       predictWebcam();
@@ -60,7 +48,38 @@ const enableWebcam = async () => {
   });
 };
 
-const drawBoundingBox = (keypoints) => {
+const createHandLandmarker = async () => {
+  const vision = await FilesetResolver.forVisionTasks(
+    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
+  );
+  handLandmarker = await HandLandmarker.createFromOptions(vision, {
+    baseOptions: {
+      modelAssetPath:
+        "https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task",
+      delegate: "GPU",
+    },
+    runningMode: runningMode,
+    numHands: 1,
+    minHandDetectionConfidence: 0.8,
+    minTrackingConfidence: 0.8,
+    minHandPresenceConfidence: 0.8,
+  });
+  enableWebcam();
+};
+
+createHandLandmarker();
+
+const drawCanvasContext = (keypoints) => {
+  canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+
+  canvasCtx.fillStyle = "blue";
+  for (let i = 0; i < keypoints.length; i++) {
+    const x = keypoints[i].x * canvas.width;
+    const y = keypoints[i].y * canvas.height;
+    canvasCtx.beginPath();
+    canvasCtx.arc(x, y, 5, 0, 2 * Math.PI);
+    canvasCtx.fill();
+  }
   let minX = Infinity;
   let maxX = -Infinity;
   let minY = Infinity;
@@ -75,35 +94,21 @@ const drawBoundingBox = (keypoints) => {
     maxY = Math.max(maxY, y);
   }
 
-  canvasCtx.strokeStyle = 'green';
+  canvasCtx.strokeStyle = "green";
   canvasCtx.lineWidth = 4;
   canvasCtx.strokeRect(minX, minY, maxX - minX, maxY - minY);
   if (predictedLetter) {
-    canvasCtx.font = '18px Arial';
-    canvasCtx.fillStyle = 'yellow';
+    canvasCtx.font = "36px Arial";
+    canvasCtx.fillStyle = "red";
     const textWidth = canvasCtx.measureText(predictedLetter).width;
     canvasCtx.fillText(predictedLetter, maxX - textWidth, minY);
   }
-}
-
-const drawHandKeypoints = (keypoints) => {
-  canvasCtx.fillStyle = "blue"; 
-  for (let i = 0; i < keypoints.length; i++) {
-    const x = keypoints[i].x * canvas.width;
-    const y = keypoints[i].y * canvas.height;
-    canvasCtx.beginPath();
-    canvasCtx.arc(x, y, 5, 0, 2 * Math.PI);
-    canvasCtx.fill();
-  }
-  drawBoundingBox(keypoints);
-}
-
-let lastVideoTime = -1;
-let results = undefined;
+};
 
 const predictWebcam = async () => {
-
   let startTimeMs = performance.now();
+  let keypoints;
+  let prediction;
   if (lastVideoTime !== video.currentTime) {
     lastVideoTime = video.currentTime;
     results = await handLandmarker.detectForVideo(video, startTimeMs);
@@ -111,43 +116,39 @@ const predictWebcam = async () => {
 
   if (results && results.landmarks && results.landmarks.length > 0) {
     const dataUrl = canvas.toDataURL("image/jpeg", 1.0);
-    const keypoints = results.landmarks[0];
-    const prediction = await sendKeypointsToBackend(keypoints, dataUrl);
-    
-    if (prediction && prediction.letter) {
-      // canvasCtx.save();
-      predictedLetter = prediction.letter;
-      drawHandKeypoints(keypoints);
-      // canvasCtx.restore();
-    }
-
+    keypoints = results.landmarks[0];
+    prediction = await sendKeypointsToBackend(keypoints, dataUrl);
+    predictedLetter = prediction.letter;
+    drawCanvasContext(keypoints);
+  } else {
+    canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
   }
+
   if (webcamRunning === true) {
     window.requestAnimationFrame(predictWebcam);
-    context.clearRect(0, 0, canvas.width, canvas.height);
   }
-}
+};
 
 const sendKeypointsToBackend = async (keypoints, dataUrl) => {
-  
-  const formData = new FormData();  
+  const formData = new FormData();
   const blob = new Blob([JSON.stringify(dataUrl)], { type: "image/jpeg" });
 
-  formData.append('keypoints', JSON.stringify(keypoints));
-  formData.append('image', blob, 'image.jpg');
+  formData.append("keypoints", JSON.stringify(keypoints));
+  formData.append("image", blob, "image.jpg");
 
   try {
     // const response = await fetch(BASE_URL + '/predict', {
-      const response = await fetch('/predict', {
-      method: 'POST',
-      body: formData
+    const response = await fetch("/predict", {
+      method: "POST",
+      body: formData,
     });
     if (!response.ok) {
-      throw new Error('Error: ' + response.status);
+      throw new Error("Error: " + response.status);
     }
     const data = await response.json();
+    console.log(data);
     return data;
   } catch (error) {
-    console.log('Erreur :', error);
+    console.log("Erreur :", error);
   }
-}
+};
